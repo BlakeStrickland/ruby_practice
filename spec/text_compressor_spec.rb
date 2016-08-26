7a62bfa087fe1c5c36ff8e8b55fcb6fe2f35c551
Blake Strickland <Stricklandb02@gmail.com>
2016-08-26 15:52:29 -0400

    Playing with rspec

---
+++ spec/text_compressor_spec.rb
@@ -0,0 +1,25 @@
+require '../text_compressor.rb'
+
+describe TextCompressor do
+
+  it "should be able to add some text" do
+    c = TextCompressor.new('')
+    c.add_text( 'first second')
+    c.unique.should == ['first', 'second']
+    c.index.should == [0, 1]
+  end
+
+  it "should be able to add a word" do
+    c = TextCompressor.new('')
+    c.add_text( 'first')
+    c.unique.should == ['first']
+    c.index.should == [0]
+  end
+
+  it "should be able to add some text" do
+    c = TextCompressor.new('hellow world')
+    c.unique_index_of ('hello').should == 0
+    c.unique_index_of ('world').should == 1
+  end
+
+end
