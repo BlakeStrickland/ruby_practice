21a216b6a415dc8fcc6bce7880f86041ec1855dd
Blake Strickland <Stricklandb02@gmail.com>
2016-08-27 18:24:34 -0400

    Print testing and refactoring

--- reading_files.rb
+++ reading_files.rb
@@ -1,5 +1,12 @@
 require "csv"
 
+def clean_data data
+  csv = data.to_csv( row_sep: nil )
+  csv.gsub! /\n/, ''
+  csv.gsub! /,"",/, ' + '
+  return cleaned_data = csv.split(" + ")
+end
+
 def return_array_from_file file
   data = []
   File.open(file, "r") do |f|
@@ -7,21 +14,30 @@ def return_array_from_file file
        data << line
     end
   end
-  data
+  return clean_data( data )
 end
 
-def clean_data data
-  csv = data.to_csv( row_sep: nil )
-  csv.gsub! /\n/, ''
-  csv.gsub! /,"",/, ' + '
-
-  cleaned_data = csv.split(" + ")
+def my_print array
+  array.each do |line|
+    puts line
+  end
+  puts "\n\n"
 end
 
-tech_data = return_array_from_file("nms_data/nms_technology.txt")
-energy_data = return_array_from_file("nms_data/nms_energy_sources.txt")
-element_data = return_array_from_file("nms_data/nms_elements.txt")
-cleaned_data = clean_data(element_data)
+alloys = return_array_from_file("nms_data/nms_alloys.txt")
+elements  = return_array_from_file("nms_data/nms_elements.txt")
+energy = return_array_from_file("nms_data/nms_energy_sources.txt")
+exo = return_array_from_file("nms_data/nms_exo_suit_enhance.txt")
+multi = return_array_from_file("nms_data/nms_multitool_enhance.txt")
+ship = return_array_from_file("nms_data/nms_ship_enhance.txt")
+tech = return_array_from_file("nms_data/nms_technology.txt")
+trade = return_array_from_file("nms_data/nms_trade_com.txt")
 
-
-puts cleaned_data
+my_print alloys
+my_print elements
+my_print energy
+my_print exo
+my_print multi
+my_print ship
+my_print tech
+my_print trade
